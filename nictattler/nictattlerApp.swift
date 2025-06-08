7420cb4178a7355daa6acf8785c0426f3cf9ba76
Saurish Srivastava <saurishs1215@gmail.com>
2025-06-08 11:30:51 -0700

    Initial Commit

---
+++ nictattler/nictattlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  nictattlerApp.swift
+//  nictattler
+//
+//  Created by saurish on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct nictattlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
